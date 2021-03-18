86c7c576dae839b458c8787c36cdac48637a4952
VictorSilva15 <victor.silvaPC@hotmail.com>
2021-03-18 13:15:24 -0300

    page whit Style

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
+import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Olá React</h1>, document.getElementById("root"));
+ReactDOM.render(<h1>Olá React!</h1>, document.getElementById("root"));
